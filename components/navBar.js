c1fd383e27f857768b78de4c59e9d741fee4cd40
sevalyy <seval@yamanyar.com>
2022-11-01 11:52:09 +0100

    logo added to the navbar

--- components/navBar.js
+++ components/navBar.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 function NavBar() {
   return (
     <div className={styles.navbar}>
+      {" "}
       <Link href="/">Homepage</Link>
+      <Image src="/Rick_and_Morty.png" alt="RaM logo" width={250} height={50} />
       <Link href="/about">About Us </Link>
     </div>
   );
